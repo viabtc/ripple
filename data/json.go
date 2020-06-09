080cedcee594950c4d24dec9d7f85f1e29ea94c2
jingniao <gh0506@outlook.com>
2020-06-09 09:49:32 +0800

    碰到未预定义的交易或者ledger对象时不返回错误

--- data/json.go
+++ data/json.go
@@ -277,7 +277,9 @@ func (r *TransactionResult) UnmarshalText(b []byte) error {
 		*r = result
 		return nil
 	}
-	return fmt.Errorf("Unknown TransactionResult: %s", string(b))
+	*r = tesUNKNOWN_TYPE
+	return nil
+
 }
 
 func (l LedgerEntryType) MarshalText() ([]byte, error) {
@@ -289,8 +291,8 @@ func (l *LedgerEntryType) UnmarshalText(b []byte) error {
 		*l = leType
 		return nil
 	}
-	// If here, add tx type to TxFactory and TxTypes in factory.go
-	return fmt.Errorf("Unknown LedgerEntryType: %s", string(b))
+	*l = UNKNOW_LEDGER_TYPE
+	return nil
 }
 
 func (t TransactionType) MarshalText() ([]byte, error) {
@@ -302,8 +304,8 @@ func (t *TransactionType) UnmarshalText(b []byte) error {
 		*t = txType
 		return nil
 	}
-	// If here, add tx type to TxFactory and TxTypes in factory.go
-	return fmt.Errorf("Unknown TransactionType: %s", string(b))
+	*t = UNKNOW_TX_TYPE
+	return nil
 }
 
 func (t RippleTime) MarshalJSON() ([]byte, error) {
